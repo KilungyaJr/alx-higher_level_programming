22998829d75ab4670b8d2e702339a3a49d73dd11
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-09-26 16:24:45 +0300

    fixed return on an empty list

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     first = sentence[0]
     length = len(sentence)
     if length == 0:
-        return (0, "None")
+        return (0, None)
     else:
         return (length, first)
