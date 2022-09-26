57a6fb80dce48468de37fd6115f1e53bc4945906
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-09-26 16:05:48 +0300

    changed return code

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,4 +10,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = 0, 0
         else:
             tuple_b = tuple_b[0], 0
-    return tuple(sum(i) for i in zip(tuple_a, tuple_b))
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
