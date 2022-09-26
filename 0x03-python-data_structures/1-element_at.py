048584c9953993e06d8481461882202d2d18e982
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-09-26 13:45:52 +0300

    fixed return value

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     for i in my_list:
         if idx < 0 or idx > len(my_list):
-            return None
+            return 'None'
         else:
             return my_list[idx]
