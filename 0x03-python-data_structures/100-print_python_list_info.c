9816079b91b331dc6268023a36f67d9496b2fa26
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-09-27 00:03:54 +0300

    100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include
+
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: pointer to PyObject list
+ *
+ */
+void print_python_list_info(PyObject *p)
+{
+	/* ... */
+}
