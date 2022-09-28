71cfb84e39180d8b33954fd09ed368bed204cd8d
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-09-28 17:41:35 +0300

    handled not a string and none return value

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,9 @@ def roman_to_int(roman_string):
     nums = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     ans = 0
 
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
     for i in range(len(roman_string)):
         if i+1 != len(roman_string) and\
            nums[roman_string[i]] < nums[roman_string[i+1]]:
