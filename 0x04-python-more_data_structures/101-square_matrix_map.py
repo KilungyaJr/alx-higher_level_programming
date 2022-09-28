041e04beb46a292a1fb522f5aba25714d3d01e27
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-09-28 18:06:00 +0300

    used map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    sq_matrix = []
-    for x in matrix:
-        sq_matrix.append(list(map(lambda x: x ** 2, x)))
-    return sq_matrix
+    return list(map(lambda a: list(map(lambda i: i ** 2, a)), matrix))
