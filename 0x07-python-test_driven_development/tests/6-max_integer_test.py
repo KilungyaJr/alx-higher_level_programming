12864e0bb02ad3f72a74e918e2931ca4209332ed
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-10-19 12:34:48 +0300

    updated testcase

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Unittest for max_integer([..])
 """
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
+
 
 class TestMaxInteger(unittest.TestCase):
     """provides test cases for max_integer func"""
