232d67e8da2ac500c4c2fc6c8b9b6375a11ae88c
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-10-20 14:45:05 +0300

    updated test cases

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -18,10 +18,7 @@ def print_square(size):
     if size is float and size < 0:
         raise TypeError("size must be an integer")
 
-    if size == 0:
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
         print()
-    else:
-        for i in range(size):
-            for j in range(size):
-                print("#", end="")
-            print()
