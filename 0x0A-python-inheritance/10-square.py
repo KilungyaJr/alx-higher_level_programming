86b19bbcdbc6efc9ba1f8a4d71a2e5b8da215985
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-10-24 23:08:01 +0300

    implementing a square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Defines a class Square
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """represents a Square that inherits Rectangle class
+
+    Attributes:
+    size - private
+    """
+    def __init__(self, size):
+        """initializes data
+
+        Args:
+        size: size of a side of a square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """finds the area of a square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """prints square description"""
+        return str("[Rectangle] {:d}/{:d}".format(self.__size, self.__size))
