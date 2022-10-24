ffb4c5d6d20309466efc62230b8598382d931ecf
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-10-24 21:57:37 +0300

    raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines a class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """public instance method: area"""
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
