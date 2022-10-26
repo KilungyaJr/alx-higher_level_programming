67093c95d2ff4e19c5bb77e18a1ded82690bc9b2
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-10-26 11:21:04 +0300

    updated func

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ Module: read_file
 
 def read_file(filename=""):
     """reads a text file and prints it to stdout"""
-    with open("my_file_0.txt", encoding="utf-8") as filename:
-        read_data = filename.read()
+    with open(filename, encoding="utf-8") as my_file:
+        read_data = my_file.read()
         print(read_data, end="")
