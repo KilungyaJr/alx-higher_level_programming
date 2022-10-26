9a670b3c0b22d39f2dccdade40d38e63518d22e3
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-10-26 13:47:14 +0300

    replaces all attributes of the Student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Defines a class Student
+"""
+
+
+class Student():
+    """a student class
+
+    Attributes:
+    first_name: student's first name, public
+    last_name: student's last name, public
+    age: student's age, public
+    """
+    def __init__(self, first_name, last_name, age):
+        """initializes the data"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            my_dict = {}
+            if type(attrs) is list and all(type(i) is str for i in attrs):
+                for i in attrs:
+                    if i in self.__dict__:
+                        my_dict.update({i: self.__dict__[i]})
+                return my_dict
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance
+
+        Args:
+        json: dictionary of attributes
+        """
+        for i in json:
+            self.__dict__.update({i: json[i]})
