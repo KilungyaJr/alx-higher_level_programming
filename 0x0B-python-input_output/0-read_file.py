d8d0e173a20319bf72535b9874bcadfc195c903c
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-10-26 11:17:58 +0300

    added print function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,3 +8,4 @@ def read_file(filename=""):
     """reads a text file and prints it to stdout"""
     with open("my_file_0.txt", encoding="utf-8") as filename:
         read_data = filename.read()
+        print(read_data, end="")
