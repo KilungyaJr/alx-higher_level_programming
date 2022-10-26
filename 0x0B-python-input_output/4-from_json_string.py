9abb216e68510a94e6fd86f46fe995d09a063ce1
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-10-26 11:58:58 +0300

    returns an object (Python data structure) represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module: from_json_string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns:
+    an object (Python data structure) represented by a JSON string
+    """
+    return json.loads(my_str)
