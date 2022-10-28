cecf85a1417c0c5d030f46bb2e5f258f576ee7bc
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-10-28 17:54:57 +0300

    writes the JSON string representation to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,3 +28,14 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        with open(filename, mode="w", encoding="utf=8") as json_file:
+            if list_objs is None:
+                json_file.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                json_file.write(Base.to_json_string(list_dicts))
