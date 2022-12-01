500d11d6ed210dc929e33cb811095dd70e8cc175
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-12-01 18:53:47 +0300

    SQL - more queries

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,10 @@
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating
+-- Each record should display: tv_genres.name - rating sum
+-- Results must be sorted in descending order by their rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate)
+AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
