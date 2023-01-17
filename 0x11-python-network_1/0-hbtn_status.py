591150b14e73537dc6ebede5d2a644b6123bb3b4
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-01-17 15:07:21 +0300

    added docstring and nested code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""
+This module is used to fetch the status of https://alx-intranet.hbtn.io/status
+using the package urllib
+"""
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    print("Body response:\n\t- type:", type(body), "\n\t- content:", body, "\n\t- utf8 content:", body.decode('utf-8'))
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
