062418d5b27d4cadea3d71830b08b1e2b9736a73
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-01-17 09:35:01 +0300

    fixed pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -19,8 +19,8 @@ def find_peak(list_of_integers):
     if n < 3:
         return max(list_of_integers)
     mid = n // 2
-    if list_of_integers[mid - 1] < list_of_integers[mid] and
-    list_of_integers[mid] > list_of_integers[mid + 1]:
+    if list_of_integers[mid - 1] < list_of_integers[mid] \
+       and list_of_integers[mid] > list_of_integers[mid + 1]:
         return list_of_integers[mid]
     elif list_of_integers[mid - 1] < list_of_integers[mid]:
         return find_peak(list_of_integers[mid:])
