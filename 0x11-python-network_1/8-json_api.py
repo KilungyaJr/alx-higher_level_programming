002597c964c4991cc6395e80799adef0c4966294
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-01-17 16:00:13 +0300

    takes in a letter and sends a POST request

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module is used to send a POST request to http://0.0.0.0:5000/search_user
+with a letter as a parameter using the package requests and sys
+"""
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    data = {'q': q}
+    response = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    try:
+        json_res = json.loads(response.text)
+        if json_res:
+            print("[{}] {}".format(json_res.get('id'), json_res.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
