50786252585680912f5b55feeb4c207ed49b1094
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-02-09 16:19:57 +0300

    fixed semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-var fs = require('fs');
-var filePath = process.argv[2];
+const fs = require('fs');
+const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', function (err, data) {
   if (err) {
