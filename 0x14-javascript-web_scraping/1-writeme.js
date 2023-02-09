13aebc831c077867fa9ce6462f560b45c3c64283
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-02-09 16:27:01 +0300

    removed message for success

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,7 +7,5 @@ const data = process.argv[3];
 fs.writeFile(filePath, data, 'utf-8', function (err) {
   if (err) {
     console.error(err);
-    return;
   }
-  console.log('The file was saved!');
 });
