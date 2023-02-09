8995132ce980e4a1d0f6e0e6f966caa33ab52cea
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-02-09 17:07:26 +0300

    fixed code to print one by one

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,23 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, { json: true }, (err, res, body) => {
-  if (err) {
-    console.error(err);
-    return;
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-
-  body.characters.forEach((character) => {
-    request(character, { json: true }, (err, res, body) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-
-      console.log(body.name);
-    });
-  });
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
