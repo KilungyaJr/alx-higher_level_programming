da43e538bf2ae2dcb2635142744a81270a0ca9ae
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-02-09 16:30:48 +0300

    display the status code of a GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log('code: ' + response.statusCode);
+});
